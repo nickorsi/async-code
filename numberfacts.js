9229bcff949b0d376d561a08bbea440d3b575456
Teagan McClintock <teagan.mcclintock@gmail.com>
2023-12-31 08:38:50 -0500

    Resolved issue with Race function and started work on All

--- numberfacts.js
+++ numberfacts.js
@@ -1,10 +1,12 @@
 "use strict";
 
+const NUMBERS_API_URL = "http://numbersapi.com";
+
 
 /**showNumberTrivia takes a whole number "num" and console logs trivia about
  * this number */
 async function showNumberTrivia(num) {
-  const response = await fetch(`http://numbersapi.com/${num}?json`);
+  const response = await fetch(`${NUMBERS_API_URL}/${num}?json`);
 
   const responseJson = await response.json();
 
@@ -17,12 +19,12 @@ async function showNumberTrivia(num) {
  * requests for trivia associated with each number, and console logs trivia
  * associated with the first response. */
 async function showNumberRace(num1, num2, num3, num4) {
-  const resp1 = await fetch(`http://numbersapi.com/${num1}?json`);
-  const resp2 = await fetch(`http://numbersapi.com/${num2}?json`);
-  const resp3 = await fetch(`http://numbersapi.com/${num3}?json`);
-  const resp4 = await fetch(`http://numbersapi.com/${num4}?json`);
+  const promise1 = fetch(`${NUMBERS_API_URL}/${num1}?json`);
+  const promise2 = fetch(`${NUMBERS_API_URL}/${num2}?json`);
+  const promise3 = fetch(`${NUMBERS_API_URL}/${num3}?json`);
+  const promise4 = fetch(`${NUMBERS_API_URL}/${num4}?json`);
 
-  const firstResp = await Promise.race([resp1, resp2, resp3, resp4]);
+  const firstResp = await Promise.race([promise1, promise2, promise3, promise4]);
 
   const firstRespJson = await firstResp.json();
 
@@ -31,4 +33,16 @@ async function showNumberRace(num1, num2, num3, num4) {
   console.log(firstNumTrivia);
 }
 
+/**showNumberAll takes an array of inputs (should be numbers but at least one
+ * will not be), makes a simultaneous request for trivia about each input, and
+ * console logs the array of trivia that had a successful response, then console
+ * logs the array of error messages for failed responses */
+async function showNumberAll(inputs) {
+  const promises = inputs.map(input => fetch(`${NUMBERS_API_URL}/${input}?json`));
+
+  const responses = await Promise.allSettled(promises);
+
+  console.log(responses);
+
+}
 
