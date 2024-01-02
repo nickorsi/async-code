887bd7a364afebe50f285c638114c00234448f34
Nicholas <nick.orsi1992@gmail.com>
2024-01-01 17:46:00 -0800

    Finished adding functionality to the file to complete part 1.

--- numberfacts.js
+++ numberfacts.js
@@ -5,6 +5,7 @@ const NUMBERS_API_URL = "http://numbersapi.com";
 
 /**showNumberTrivia takes a whole number "num" and console logs trivia about
  * this number */
+
 async function showNumberTrivia(num) {
   const response = await fetch(`${NUMBERS_API_URL}/${num}?json`);
 
@@ -18,6 +19,7 @@ async function showNumberTrivia(num) {
 /**showNumberRace takes 4 whole numbers "num1" "num2" "num3" "num4", makes
  * requests for trivia associated with each number, and console logs trivia
  * associated with the first response. */
+
 async function showNumberRace(num1, num2, num3, num4) {
   const promise1 = fetch(`${NUMBERS_API_URL}/${num1}?json`);
   const promise2 = fetch(`${NUMBERS_API_URL}/${num2}?json`);
@@ -37,12 +39,39 @@ async function showNumberRace(num1, num2, num3, num4) {
  * will not be), makes a simultaneous request for trivia about each input, and
  * console logs the array of trivia that had a successful response, then console
  * logs the array of error messages for failed responses */
+
 async function showNumberAll(inputs) {
   const promises = inputs.map(input => fetch(`${NUMBERS_API_URL}/${input}?json`));
 
-  const responses = await Promise.allSettled(promises);
+  const promiseResults = await Promise.allSettled(promises);
 
-  console.log(responses);
+  // console.log(promiseResults);
 
+  const successfulTrivia = [];
+
+  const errorMessages = [];
+
+  // TODO: How to do this with an idiom function?
+  // Use .map(async namedFunction) with use of await in the namedFunction?
+  for(const promiseResult of promiseResults) {
+    // console.log(promiseResult.value.status);
+    if(promiseResult.value.status === 200){
+      const responseJson = await promiseResult.value.json();
+      successfulTrivia.push(responseJson.text);
+    }
+    else {
+      errorMessages.push(promiseResult.value.statusText);
+    }
+  }
+  console.log("fulfilled: ", successfulTrivia);
+  console.log("rejected: ", errorMessages);
 }
 
+/** main function will call all three functions above and fully completes each
+ * function prior to moving on to the next.  */
+
+async function main() {
+  await showNumberTrivia(5);
+  await showNumberRace(1,3,4,42);
+  await showNumberAll([7, 11, 22, "wrong"]);
+}
