0b05fcf7f552650f0bea05342ee2721ae596a4cf
Nicholas <nick.orsi1992@gmail.com>
2024-01-01 18:04:26 -0800

    Added code review comments.

--- numberfacts.js
+++ numberfacts.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const NUMBERS_API_URL = "http://numbersapi.com";
+const NUMBERS_API_BASE_URL = "http://numbersapi.com";
 
 
 /**showNumberTrivia takes a whole number "num" and console logs trivia about
  * this number */
 
 async function showNumberTrivia(num) {
-  const response = await fetch(`${NUMBERS_API_URL}/${num}?json`);
+  const response = await fetch(`${NUMBERS_API_BASE_URL}/${num}?json`);
 
   const responseJson = await response.json();
 
@@ -21,11 +21,11 @@ async function showNumberTrivia(num) {
  * associated with the first response. */
 
 async function showNumberRace(num1, num2, num3, num4) {
-  const promise1 = fetch(`${NUMBERS_API_URL}/${num1}?json`);
-  const promise2 = fetch(`${NUMBERS_API_URL}/${num2}?json`);
-  const promise3 = fetch(`${NUMBERS_API_URL}/${num3}?json`);
-  const promise4 = fetch(`${NUMBERS_API_URL}/${num4}?json`);
-
+  const promise1 = fetch(`${NUMBERS_API_BASE_URL}/${num1}?json`);
+  const promise2 = fetch(`${NUMBERS_API_BASE_URL}/${num2}?json`);
+  const promise3 = fetch(`${NUMBERS_API_BASE_URL}/${num3}?json`);
+  const promise4 = fetch(`${NUMBERS_API_BASE_URL}/${num4}?json`);
+  // TODO: Another option, use map to replace lines 24-27 to create a promise array
   const firstResp = await Promise.race([promise1, promise2, promise3, promise4]);
 
   const firstRespJson = await firstResp.json();
@@ -41,7 +41,8 @@ async function showNumberRace(num1, num2, num3, num4) {
  * logs the array of error messages for failed responses */
 
 async function showNumberAll(inputs) {
-  const promises = inputs.map(input => fetch(`${NUMBERS_API_URL}/${input}?json`));
+  const promises = inputs.map(
+    input => fetch(`${NUMBERS_API_BASE_URL}/${input}?json`));
 
   const promiseResults = await Promise.allSettled(promises);
 
@@ -53,9 +54,13 @@ async function showNumberAll(inputs) {
 
   // TODO: How to do this with an idiom function?
   // Use .map(async namedFunction) with use of await in the namedFunction?
-  for(const promiseResult of promiseResults) {
+  // Use filter to pull out all successful results, then promise.all to get all
+  // of the different responses back.
+  // TODO: Add another check for a "fullfilled" status on the promiseResult object
+  // otherwise the below check wouldn't work if the status was rejected.
+  for (const promiseResult of promiseResults) {
     // console.log(promiseResult.value.status);
-    if(promiseResult.value.status === 200){
+    if (promiseResult.value.status === 200) {
       const responseJson = await promiseResult.value.json();
       successfulTrivia.push(responseJson.text);
     }
@@ -72,6 +77,6 @@ async function showNumberAll(inputs) {
 
 async function main() {
   await showNumberTrivia(5);
-  await showNumberRace(1,3,4,42);
+  await showNumberRace(1, 3, 4, 42);
   await showNumberAll([7, 11, 22, "wrong"]);
 }
